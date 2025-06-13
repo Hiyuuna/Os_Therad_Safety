327a288f0c6780ebbf82ae9b3ef65f22bda26c0f
Hiyuuna <dazein31@gmail.com>
2025-06-13 16:56:52 +0900

    헤더, cpp파일 통합

--- Project7/qtype.h
+++ Project7/qtype.h
@@ -1,6 +1,8 @@
 #ifndef QTYPE_H
 #define QTYPE_H
 
+#include <mutex>
+
 typedef int Key;
 
 struct Item {
@@ -22,5 +24,14 @@ struct Queue {
     Node* head;
     Node* tail;
 };
+
+// 함수 선언
+Queue* init(void);
+void release(Queue* q);
+Reply enqueue(Queue* q, Item it);
+Reply dequeue(Queue* q);
+Queue* range(Queue* q, Key s, Key e);
+Node* nalloc(Item it);
+void nfree(Node* node);
 
 #endif
--- Project7/queue.cpp
+++ Project7/queue.cpp
@@ -1,5 +1,8 @@
-#include "queue.h"
-#include <mutex>
+#include "qtype.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <thread>
 
 using namespace std;
 
@@ -38,7 +41,7 @@ Reply enqueue(Queue* q, Item it) {
     Reply rp{ false, {} };
 
     Node* n = nalloc(it);
-    if (n == nullptr) return rp;
+    if (!n) return rp;
 
     if (q->head == nullptr) {
         q->head = q->tail = n;
@@ -46,20 +49,19 @@ Reply enqueue(Queue* q, Item it) {
     else {
         Node* prev = nullptr;
         Node* curr = q->head;
-
-        while (curr != nullptr && curr->item.key <= it.key) {
+        while (curr && curr->item.key <= it.key) {
             prev = curr;
             curr = curr->next;
         }
 
-        if (prev == nullptr) {
+        if (!prev) {
             n->next = q->head;
             q->head = n;
         }
         else {
             prev->next = n;
             n->next = curr;
-            if (curr == nullptr) q->tail = n;
+            if (!curr) q->tail = n;
         }
     }
 
@@ -72,11 +74,11 @@ Reply dequeue(Queue* q) {
     lock_guard<mutex> lk(qmtx);
     Reply rp{ false, {} };
 
-    if (q->head == nullptr) return rp;
+    if (!q->head) return rp;
 
     Node* del = q->head;
     q->head = del->next;
-    if (q->head == nullptr) q->tail = nullptr;
+    if (!q->head) q->tail = nullptr;
 
     rp.success = true;
     rp.item = del->item;
@@ -89,7 +91,7 @@ Queue* range(Queue* q, Key s, Key e) {
     Queue* res = init();
     Node* p = q->head;
 
-    while (p != nullptr) {
+    while (p) {
         if (p->item.key >= s && p->item.key <= e) {
             enqueue(res, p->item);
         }
@@ -97,4 +99,46 @@ Queue* range(Queue* q, Key s, Key e) {
     }
 
     return res;
+}
+
+void producer(Queue* q, int id) {
+    for (int i = 0; i < 3; ++i) {
+        Item it;
+        it.key = rand() % 500;
+        it.value = id * 10 + i;
+        enqueue(q, it);
+    }
+}
+
+void consumer(Queue* q, int id) {
+    for (int i = 0; i < 3; ++i) {
+        Reply r = dequeue(q);
+        if (r.success)
+            cout << "[T" << id << "] Got: " << r.item.key << ", " << r.item.value << endl;
+    }
+}
+
+int main() {
+    srand((unsigned int)time(NULL));
+    Queue* q = init();
+
+    thread t1(producer, q, 1);
+    thread t2(producer, q, 2);
+    thread t3(consumer, q, 3);
+    thread t4(consumer, q, 4);
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+
+    Queue* r = range(q, 100, 300);
+    cout << "--- range ---" << endl;
+    Reply x;
+    while ((x = dequeue(r)).success)
+        cout << x.item.key << ", " << x.item.value << endl;
+
+    release(q);
+    release(r);
+    return 0;
 }
